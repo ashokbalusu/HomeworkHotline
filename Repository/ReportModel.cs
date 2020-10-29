6893484ec21f25deb3e33122735c68d5a28eedde
Casey Curtis <BOC9203@HCA.corpad.net>
2020-10-29 12:07:03 -0500

    Report Backend

---
+++ Repository/ReportModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class ReportModel
+    {
+        public int EntityId { get; set; }
+        public string EntityName { get; set; }
+        public string TotalMinutes { get; set; }
+        public string TotalDollars { get; set; }
+        public int SeesionsPreviousYearComparison { get; set; }
+        public string PreviousYearRange { get; set; }
+        public string CurrentYearRange { get; set; }
+        public string QuarterMidYearName { get; set; }
+        public string ReportEndDate { get; set; }
+        public int TotalSessions { get; set; }
+        public int TotalIndividualStudents { get; set; }
+        public int TotalIndividualParents { get; set; }
+        public int TotalMinutesFreeTutoring { get; set; }
+        public int TotalTeacherPositionsPerWeek { get; set; }
+        public string DistrictTutoringHourCost { get; set; }
+        public string DistrictTutoringHourlyRate { get; set; }
+        public int DistirctPromotionalItemStudents { get; set; }
+        public string DistirctPromotionalItemCost { get; set; }
+        public double DistirctPhonesPercentOfUsage { get; set; }
+        public double DistirctPhonesCost { get; set; }
+        public List<ChartModel> StudentsAndSessions { get; set; }
+        public List<ChartModel> SessionResults { get; set; }
+        public List<ChartModel> SubjectBreakdown { get; set; }
+        public List<ChartModel> SessionsPerGrade { get; set; }
+        public List<ChartModel> SchoolSessionsStudentGrid { get; set; }
+    }
+}
---
+++ Repository/ReportService.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repository;
+using System.Web;
+using System.Data;
+using System.Net.Http;
+using System.Data.Entity;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Data.SqlClient;
+using System.Data.Entity.Infrastructure;
+using Microsoft.SqlServer.Server;
+using System.IO;
+using System.IO.Compression;
+
+namespace Repository
+{
+
+    public class ReportService
+    {
+        private HomeworkHotlineEntities entities;
+
+        public ReportService(HomeworkHotlineEntities entities)
+        {
+            this.entities = entities;
+        }
+
+        public List<ReportModel> GetReportData(string startDate, string endDate, List<int> countyIds = null)
+        {
+            var reportData = new List<ReportModel>();
+            entities.Database.Initialize(force: false);
+
+            var cmd = entities.Database.Connection.CreateCommand();
+            cmd.CommandText = "[dbo].[usp_ReportSummary]";
+
+            var startDateParam = new SqlParameter("StartDate", SqlDbType.VarChar);
+            startDateParam.Value = startDate;
+            cmd.Parameters.Add(startDateParam);
+
+            var endDateParam = new SqlParameter("EndDate", SqlDbType.VarChar);
+            endDateParam.Value = endDate;
+            cmd.Parameters.Add(endDateParam);
+
+            if (countyIds != null && countyIds.Count > 0)
+            {
+                var countyIdTableSchema = new List<SqlMetaData>(1)
+                    {
+                            new SqlMetaData("CountyId", SqlDbType.Int),
+                            new SqlMetaData("CountyName", SqlDbType.VarChar)
+                     }.ToArray();
+
+                var countyIdTable = new List<SqlDataRecord>();
+
+                foreach (var countId in countyIds)
+                {
+                    var tableRow = new SqlDataRecord(countyIdTableSchema);
+                    tableRow.SetValue(0, countId);
+                    tableRow.SetValue(1, "");
+                    countyIdTable.Add(tableRow);
+                }
+
+                var countyIdParam = new SqlParameter
+                {
+                    SqlDbType = SqlDbType.Structured,
+                    Direction = ParameterDirection.Input,
+                    ParameterName = "CountyIds",
+                    TypeName = "[dbo].[CountyList]",
+                    Value = countyIdTable
+                };
+
+                endDateParam.Value = endDate;
+                cmd.Parameters.Add(endDateParam);
+            }
+
+            try
+            {
+                entities.Database.Connection.Open();
+                var reader = cmd.ExecuteReader();
+
+
+
+                reader.NextResult();
+            }
+            finally
+            {
+                entities.Database.Connection.Close();
+            }
+
+            return reportData;
+        }
+
+        public Stream GetReportZip(List<ReportModel> reportData) {
+            var outStream = new MemoryStream();
+
+            try
+            {
+                using (ZipArchive archive = new ZipArchive(outStream, ZipArchiveMode.Create, true))
+                {
+                    var entityIds = reportData.Select(p => p.EntityId).Distinct().OrderBy(c => c);
+                    foreach (var entityId in entityIds)
+                    {
+
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                outStream.Close();
+            }
+
+            return outStream;
+        }
+
+        public Stream GenerateReportDocX(ReportModel reportData)
+        {
+            var docxStream = new MemoryStream();
+            return docxStream;
+        }
+
+        public void Dispose()
+        {
+            entities.Dispose();
+        }
+
+    }
+}
